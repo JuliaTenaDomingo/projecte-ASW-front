9b5bc744188d8227786f74d73254dbafe53bb3c2
agusti.costabella <agusti.costabella@estudiantat.upc.edu>
2024-05-21 14:13:45 +0200

    crida al backend modificada

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import userStore from './stores/userStore.js'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-const app = createApp(App).use(router).use(ElementPlus)
+const app = createApp(App).use(router).use(ElementPlus).use(userStore)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
