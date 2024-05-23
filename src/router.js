fd6b79321bc39271f635c11d5e21a3b523cc0ed1
albalr <albalopez656@gmail.com>
2024-05-23 19:39:06 +0200

    tasca finalitzada es creen posts

--- src/router.js
+++ src/router.js
@@ -6,10 +6,12 @@ import Magazine from '@/views/Magazine.vue'
 import User from '@/views/User.vue'
 import EditPost from '@/views/EditPost.vue'
 import NewMagazine from "@/views/NewMagazine.vue";
+import NewPost from "@/views/NewPost.vue";
 
 const routes = [
   {path: '/', name: 'Posts', component: Posts},
   {path: '/posts/:post_id', name: 'Post', component: Post},
+  {path: '/posts/new', name: 'NewPost', component: NewPost},
   {path: '/magazines', name: 'Magazines', component: Magazines},
   {path: '/magazines/new', name: 'NewMagazine', component: NewMagazine},
   {path: '/magazines/:magazine_id', name: 'Magazine', component: Magazine},
